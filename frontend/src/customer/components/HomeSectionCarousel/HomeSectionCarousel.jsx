25d6e289979bd6baa13e63fb6efb63a373a10956
iam-riyaz <97458162+iam-riyaz@users.noreply.github.com>
2023-11-29 00:17:33 +0530

    carousel design fixed

--- frontend/src/customer/components/HomeSectionCarousel/HomeSectionCarousel.jsx
+++ frontend/src/customer/components/HomeSectionCarousel/HomeSectionCarousel.jsx
@@ -1,43 +1,62 @@
 import AliceCarousel from "react-alice-carousel";
 import { HomeSectionCart } from "../HomeSectionCard/HomeSectionCard";
 import { Button } from "@mui/material";
-import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
-import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+import { useState } from "react";
+import { mens_kurta } from "../../../data/mens_kurta";
 
 export const HomeSectionCarousel = () => {
-    const responsive = {
-        0: { items: 1 },
-        568: { items: 2 },
-        1024: { items: 4.5 },
-    };
+  const [acitveIndex, setActiveIndex] = useState(0);
 
-  const items = [1, 2, 1, 2, 3, 4, 4, 5, 6].map((item) => <HomeSectionCart />);
+  const responsive = {
+    0: { items: 1 },
+    568: { items: 2 },
+    700: { items: 3 },
+    1024: { items: 5.4 },
+  };
+
+  const slidePreview = () => setActiveIndex(acitveIndex - 1);
+
+  const slideNext = () => setActiveIndex(acitveIndex + 1);
+
+  const syncActiveIndex = ({ item }) => setActiveIndex(item);
+
+  const items = mens_kurta.map((item) => <HomeSectionCart product={item} />);
 
   return (
     <>
       <div className="relative px-2 lg:px-2 flex justify-center py-2 my-2">
-        <Button >
-            <div className=" border border-solid border-gray-200 rounded-lg">
-        <ChevronLeftIcon sx={{color:"black", margin:"20px 5px 20px 5px" }} />
-        </div>
-        </Button>
-        <div className="relative    w-11/12">
+        {acitveIndex != 0 ? (
+          <div className="flex items-center">
+            <div
+              onClick={slidePreview}
+              className="cursor-pointer border border-solid border-gray-200 h-16 px-1 mr-1 rounded-lg flex items-center"
+            >
+              <ChevronLeftIcon sx={{ color: "black" }} />
+            </div>
+          </div>
+        ) : null}
+        <div className="relative w-full">
           <AliceCarousel
-            keyboardNavigation={true}
-            disableButtonsControls
-            disableDotsControls
-           
-            mouseTracking={true}
             items={items}
+            disableButtonsControls
             responsive={responsive}
-            controlsStrategy="alternate"
+            disableDotsControls
+            onSlideChanged={syncActiveIndex}
+            activeIndex={acitveIndex}
           />
         </div>
-        <Button >
-        <div className=" border border-solid border-gray-200 rounded-lg">
-        <ChevronRightIcon sx={{color:"black", margin:"20px 5px 20px 5px" }} />
-        </div>
-        </Button>
+        {acitveIndex != items.length - 1 ? (
+          <div className="flex items-center">
+            <div
+              onClick={slideNext}
+              className="cursor-pointer border border-solid border-gray-200 h-16 px-1 ml-1 rounded-lg flex items-center"
+            >
+              <ChevronRightIcon sx={{ color: "black" }} />
+            </div>
+          </div>
+        ) : null}
       </div>
     </>
   );
