1089bc3b76003e272dad70c13b74d85c4b54dd78
iam-riyaz <97458162+iam-riyaz@users.noreply.github.com>
2024-01-02 23:25:19 +0530

    update

--- frontend/src/customer/pages/homePage/index.jsx
+++ frontend/src/customer/pages/homePage/index.jsx
@@ -1,21 +1,22 @@
-import { MainCarousel } from "../../components/HomeCarousel/MainCarousel"
-import { HomeSectionCarousel } from "../../components/HomeSectionCarousel/HomeSectionCarousel"
-import { mens_kurta } from "../../../data/mens_kurta"
+import { MainCarousel } from "../../components/HomeCarousel/MainCarousel";
+import { HomeSectionCarousel } from "../../components/HomeSectionCarousel/HomeSectionCarousel";
+import { mens_kurta } from "../../../data/mens_kurta";
 
-export const HomePage=()=>{
-    return(
-        <>
-        <div>
-            <MainCarousel/>
-        </div>
-        <div className="px-8 mt-10 ">
-           <HomeSectionCarousel data={mens_kurta} title={"Men's Kurta"}/>
-           <HomeSectionCarousel data={mens_kurta} title={"Men's Shoes"}/>
-           <HomeSectionCarousel data={mens_kurta} title={"Men's Shirt"}/>
-           <HomeSectionCarousel data={mens_kurta} title={"Women's Saree"}/>
-           <HomeSectionCarousel data={mens_kurta} title={"Women's Dress"}/>
-           
-        </div>
-        </>
-    )
-}
+export const HomePage = () => {
+  return (
+    <>
+      <div>
+        <MainCarousel />
+      </div>
+      <div className="px-8 mt-10 ">
+        <HomeSectionCarousel data={mens_kurta} title={"Men's Kurta"} />
+        <HomeSectionCarousel data={mens_kurta} title={"Men's Shoes"} />
+        <HomeSectionCarousel data={mens_kurta} title={"Men's Shirt"} />
+        <HomeSectionCarousel data={mens_kurta} title={"Women's Saree"} />
+        <HomeSectionCarousel data={mens_kurta} title={"Women's Dress"} />
+        <HomeSectionCarousel data={mens_kurta} title={"Women's Dress"} />{" "}
+        <HomeSectionCarousel data={mens_kurta} title={"Women's Dress"} />
+      </div>
+    </>
+  );
+};
