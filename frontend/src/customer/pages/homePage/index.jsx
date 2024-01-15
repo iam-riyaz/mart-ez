df343d8010d2097d572ff7389a3eea0f3ece69d8
iam-riyaz <97458162+iam-riyaz@users.noreply.github.com>
2024-01-15 16:47:58 +0530

    update

--- frontend/src/customer/pages/homePage/index.jsx
+++ frontend/src/customer/pages/homePage/index.jsx
@@ -14,7 +14,8 @@ export const HomePage = () => {
         <HomeSectionCarousel data={mens_kurta} title={"Men's Shirt"} />
         <HomeSectionCarousel data={mens_kurta} title={"Women's Saree"} />
         <HomeSectionCarousel data={mens_kurta} title={"Women's Dress"} />
-       
+        <HomeSectionCarousel data={mens_kurta} title={"Women's Saree"} />
+        <HomeSectionCarousel data={mens_kurta} title={"Women's Dress"} />
         
         
       </div>
