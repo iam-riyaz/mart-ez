4c3f49f165163dfd2f65e5630df5318298db4e3a
iam-riyaz <97458162+iam-riyaz@users.noreply.github.com>
2024-01-29 09:03:02 +0530

    filter and sort data are now in different file

--- frontend/src/customer/components/Product/ProductList.jsx
+++ frontend/src/customer/components/Product/ProductList.jsx
@@ -10,70 +10,10 @@ import {
 } from "@heroicons/react/24/outline";
 import ProductCard from "./ProductCard";
 import { mens_kurta } from "../../../data/mens_kurta";
+import { sortOptions,singleFilter,filters } from "./filter-and-sort";
 
-const sortOptions = [
-  { name: "Price: Low to High", href: "#", current: false },
-  { name: "Price: High to Low", href: "#", current: false },
-];
 
-const filters = [
-  {
-    id: "color",
-    name: "Color",
-    options: [
-      { value: "white", label: "White", checked: false },
-      { value: "beige", label: "Beige", checked: false },
-      { value: "blue", label: "Blue", checked: true },
-      { value: "brown", label: "Brown", checked: false },
-      { value: "green", label: "Green", checked: false },
-      { value: "purple", label: "Purple", checked: false },
-    ],
-  },
-  {
-    id: "size",
-    name: "Size",
-    options: [
-      { value: "S", label: "Small", checked: false },
-      { value: "M", label: "Medium", checked: false },
-      { value: "L", label: "Large", checked: false },
-    ],
-  },
-];
 
-const singleFilter = [
-  {
-    id: "price",
-    name: "Price",
-    options: [
-      { value: "159-399", label: "₹159 to ₹399" },
-      { value: "399-999", label: "₹399 to ₹999" },
-      { value: "999-1999", label: "₹999 to ₹1999" },
-      { value: "1999-2999", label: "₹1999 to ₹2999" },
-      { value: "2999-4999", label: "₹2999 to ₹4999" },
-    ],
-  },
-  {
-    id: "discount",
-    name: "Discount",
-    options: [
-      { value: "20", label: "20% and Above" },
-      { value: "30", label: "30% and Above" },
-      { value: "40", label: "40% and Above" },
-      { value: "50", label: "50% and Above" },
-      { value: "60", label: "60% and Above" },
-      { value: "70", label: "70% and Above" },
-      { value: "80", label: "80% and Above" },
-    ],
-  },
-  {
-    id: "stock",
-    name: "Availability",
-    options: [
-      { value: "in_stock", label: "In Stock" },
-      { value: "out_of_stock", label: "Out Of Stock" },
-    ],
-  },
-];
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(" ");
