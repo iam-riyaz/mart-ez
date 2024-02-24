c46402d190f8df88ce5f0b5dadf66dacb431b909
iam-riyaz <97458162+iam-riyaz@users.noreply.github.com>
2024-02-24 09:12:02 +0530

    delivery page updated

--- frontend/src/customer/components/Checkout/DeliveryAddressPage.jsx
+++ frontend/src/customer/components/Checkout/DeliveryAddressPage.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import AddressCard from "./AddressCard";
+import AddressForm from "./AddressForm";
 
 function DeliveryAddressPage() {
   return (
     <div>
+      
       <div className="grid grid-cols-12 gap-4 pt-5">
         {/* addresses box */}
-        <div className="lg:col-span-4 text-xl border sm:col-span-12  rounded-e-md h-[30rem] overflow-y-scroll ">
+        <div className="lg:col-span-4 text-xl border col-span-12  rounded-e-md h-[30rem] overflow-y-scroll ">
           <div className="">
             <div className="cursor-pointer h-[24rem]">
               <AddressCard />
@@ -21,14 +23,16 @@ function DeliveryAddressPage() {
         </div>
 
         {/* new address form */}
-        <div className="lg:col-span-8 border sm:col-span-12">
+        <div className="lg:col-span-8 border col-span-12">
           {/* form grid */}
 
           <div>
             <form>
             <div className="grid grid-cols-12 gap-4">
             
-             <div className="lg:col-span"></div>
+            <div className="col-span-12">
+             <AddressForm />
+             </div>
 
             </div>
 
