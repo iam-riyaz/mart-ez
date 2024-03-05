9f92c2238d67cc24c2fad4f489928cb13fd0b803
iam-riyaz <97458162+iam-riyaz@users.noreply.github.com>
2024-03-05 09:12:02 +0530

    address form page updated

--- frontend/src/customer/components/Checkout/AddressForm.jsx
+++ frontend/src/customer/components/Checkout/AddressForm.jsx
@@ -2,7 +2,26 @@ import React from 'react'
 
 function AddressForm() {
   return (
-    <div>AddressForm</div>
+    <div>
+        
+        <div className='grid grid-cols-12 gap-4 pt-5'>
+
+        {/* addresses box */}
+        <div className='lg:col-span-4 text-xl border sm:col-span-12  rounded-e-md h-[30rem] overflow-y-scroll' >
+
+    
+
+        </div>
+
+        {/* new address form */}
+        <div className='lg:col-span-8 border sm:col-span-12'>
+             
+       now hello
+        </div>
+
+        </div>
+
+    </div>
   )
 }
 
